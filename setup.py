5baea83d60a525af0280b117d8ac86268f8b88b2
meehanjohn <meehan6@illinois.edu>
2019-08-21 15:10:26 -0400

    Added setup files to .gitignore, renamed main file to __main__

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="monday-outlook-integration"
+    name="monday-outlook-integration",
     version="0.1",
     author="John Meehan",
     author_email="jmeehan@amuneal.com",
